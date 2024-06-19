5f3cd30fa646fb84380f5bec783c9467bf9defdd
liumingxin <1142209957@qq.com>
2024-06-19 16:04:54 +0800

    add New files

---
+++ MavenParentPro/LmSpringOauth/src/main/java/com/laugh/OauthApplication.java
@@ -0,0 +1,16 @@
+package com.laugh;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * @author LiuMingxin
+ * @date 2024/6/4&16:47
+ */
+@SpringBootApplication
+public class OauthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OauthApplication.class, args);
+    }
+}
---
+++ MavenParentPro/LmSpringWeb/src/main/java/com/laugh/WebApplication.java
@@ -0,0 +1,16 @@
+package com.laugh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author LiuMingxin
+ * @date 2024/6/4&11:34
+ */
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+         SpringApplication.run(WebApplication.class, args);
+    }
+}
